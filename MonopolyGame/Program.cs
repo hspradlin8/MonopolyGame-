8e2d4671ff236b173b1d452ba8942c1637fe9338
Heidi <hspradlin8@gmail.com>
2020-04-09 13:44:10 -0500

    firstCommit

---
+++ MonopolyGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MonopolyGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
